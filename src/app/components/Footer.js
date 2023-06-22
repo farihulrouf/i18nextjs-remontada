8f27f4d6e02a707d7a67ccb26b7727141d9fff86
Farihul Rouf <farihulrouf@gmail.com>
2023-06-22 14:03:14 +0700

    footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -28,9 +28,9 @@ const Footer = ({ lng }) => {
                             </Trans>
                         </h1>
                         <p className="py-6 text-sm sm:max-w-sm w-40">
-                            Adipisicing elit, sed do eiusmod
-                            tempor incidunta utiai labore et
-                            dolore magna si aliqua.
+                        <Trans i18nKey="desc1" t={t}>
+                        website information about description
+                            </Trans>
                         </p>
                         <input type="text" placeholder="Email" className="py-2 p-2 sm:mt-1 mt-4" />
                         <div className="mt-4 sm:mt-2 mb-2">
@@ -86,21 +86,29 @@ const Footer = ({ lng }) => {
                             <Link href="/">
                                 <div className="flex items-center bg-blue-900 px-4">
 
-                                    <img src='/logo.svg' className='w-16 h-auto' alt="logo" /> <span className="text-white text-2xl">emontadaa</span>
+                                    <img src='/logo.svg' className='w-16 h-auto' alt="logo" /> <span className="text-white text-2xl"></span>
                                 </div>
                             </Link>
 
 
                             <p className="text-white text-sm mt-6 mb-4 max-w-sm">
-                                Consectetur adipisicing elit sedtado eiusmod dunt
-                                ut labore et dolore magna aliqua enim minim veniami
-                                quis nostrud.
+                                <Trans i18nKey="desc" t={t}>
+                                website information about description
+                                </Trans>
                             </p>
                             <div className="flex space-x-2 items-center py-2">
-                                <AiOutlineHome size={30} color="yellow" /> <p className="ml-2">lorem ipsum </p>
+                                <AiOutlineHome size={30} color="yellow" /> <p className="ml-2">
+                                    <Trans i18nKey="address" t={t}>
+                                        lorem ipsum
+                                    </Trans>
+                                </p>
                             </div>
                             <div className="flex space-x-2 items-center py-2">
-                                <AiOutlineMail size={30} color="yellow" /> <p className="ml-2">info@remontaa.com </p>
+                                <AiOutlineMail size={30} color="yellow" /> <p className="ml-2">
+                                    <Trans i18nKey="email" t={t}>
+
+                                        info@remontaa.com
+                                    </Trans></p>
                             </div>
                             <div className="flex space-x-2 items-center py-2">
                                 <AiOutlinePhone size={30} color="yellow" /> <p className="ml-2">+628xxxxxxxxx</p>
